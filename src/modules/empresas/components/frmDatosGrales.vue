22e8391c0f41291423c70e542a4c72531fbe2f57
cahuichLuis <cawy2000@hotmail.com>
2023-11-30 13:16:38 -0500

    add#Domicilio

--- src/modules/empresas/components/frmDatosGrales.vue
+++ src/modules/empresas/components/frmDatosGrales.vue
@@ -6,7 +6,8 @@
                <fieldset>
                    <input v-model="idempresa" class="idEmpresa" type="text" name="idEmpresa" id="idEmpresa" placeholder="Id Empresa">
                    <span class="logotipo">Logotipo</span>
-                   <input class="imageLogo" type="file" name="logotipo" id="idLogotipo">
+                    <input class="imageLogo" type="file" name="logotipo" id="idLogotipo" accept="image/jpeg, image/png" @change="validateFile">
+                    <div v-if="fileTypeError" style="color: red;">Formato de archivo no válido. Se aceptan solo archivos JPG y PNG.</div>
                </fieldset> 
                <fieldset>
                    <input v-model="rfc" class="rfc" type="text" name="RFC" id="idRFC" placeholder="R. F. C." required minlength=12 maxlength=13 />
@@ -46,9 +47,48 @@
                         <option v-for="regimen in listaregimenes" :key="regimen.ClaveRegimenFiscal" :value="regimen.ClaveRegimenFiscal">{{regimen.Descripcion}}</option>
                     </select>
                </fieldset>
-               <fieldset>
+               <fieldset> 
                     <input class="nombreComercial" type="text" name="nombreComercial" id="idNombreComercial" placeholder="Nombre comercial">
                </fieldset>
+
+               <hr>
+               <h1>Domicilio</h1>
+               <fieldset>
+               <input v-model="idempresa" class="calleDomicilio" type="text" name="calleDomicilio" id="calleDomicilio" placeholder="Calle">
+               <input v-model="numExtDomicilio" class="numExtDomicilio" type="text" name="numExtDomicilio" id="numExtDomicilio" placeholder="No. Ext." required/>
+               <input v-model="numIntDomicilio" class="numIntDomicilio" type="text" name="numIntDomicilio" id="numIntdomicilio" placeholder="No. Int." required/>
+               </fieldset>
+               <fieldset>
+               <input v-model="codigoPostal" class="codigoPostal" type="text" name="codigoPostal" id="codigoPostal" placeholder="Código Postal"/>
+               <label class="labelEstados" for="estado">Estado</label>
+               <select class="claveEstado" name="estado" id="idEstado" v-model="estado">
+                        <option selected value="">Estado</option>
+                        <option v-for="optEstado in ListaEstados" :key="optEstado.ClaveEstado" :value="optEstado.ClaveEstado">{{optEstado.Descripcion}}</option>
+                    </select>
+            </fieldset>
+
+                <label class="labelMunicipio" for="municipio">Deleg./Municipio</label>
+                <select class="municipio" name="municipio" id="municipio">
+                    <option selected value="">Municipio</option>
+                    <option v-for="optMunicipio in ListaMunicipios" :key="optMunicipio.claveMunicipio" :value="optMunicipio.claveMunicipio">{{optMunicipio.Descripcion}}</option>
+                </select>
+
+               <fieldset>
+                <label class="labelCiudad" for="ciudad">Ciudad</label>
+                <select class="ciudad" name="ciudad" id="ciudad">
+                <option selected value=""></option>
+                <option value=""></option>
+            </select>
+               </fieldset>
+
+               <fieldset>
+                <label class="labelColonia" for="colonia">Colonia</label>
+                <select class="colonia" name="colonia" id="colonia">
+                <option selected value=""></option>
+                <option value=""></option>
+            </select>
+               </fieldset>
+
             </form>
         </div>
     </div>
@@ -58,40 +98,56 @@
 <script>
 import { ref, watch, computed, onMounted } from 'vue'
 
-const  { useEmpresa } = require( '../store/empresa')
+const { useEmpresa } = require('../store/empresa')
+
+function validateFile(event) {
+    const file = event.target.files[0];
+    const fileTypeError = document.getElementById('fileTypeError');
+
+    if (file) {
+        if (!['image/jpeg', 'image/png'].includes(file.type)) {
+            fileTypeError.style.display = 'block';
+            event.target.value = '';
+        } else {
+            fileTypeError.style.display = 'none';
+        }
+    }
+}
 
 export default {
 
     name: 'frmDatosGrenerales',
-    props:{
-        esnuevo:         Boolean,
-        idEmpresa:       String,
-        nombrecomercial: String,        
-        nombreoficial:   String,
-        pais:            String,
-        personafisca:    Boolean,
-        personamoral:    Boolean,
-        regimenfiscal:   String,
-        rfc:             String,
-        taxid:           String,
+    props: {
+        esnuevo: Boolean,
+        idEmpresa: String,
+        nombrecomercial: String,
+        nombreoficial: String,
+        pais: String,
+        personafisca: Boolean,
+        personamoral: Boolean,
+        regimenfiscal: String,
+        rfc: String,
+        taxid: String,
     },
 
-    
-    
-    setup( props ){
 
-        const esextranjejo    = ref( false )
-        const idempresa       = ref('')
-        const listaregimenes  = ref( [])
-        const nombrecomercial = ref( '' )
-        const nombreoficial   =  ref('')
-        const pais            = ref('')
-        const personafisica   = ref( true )
-        const personamoral    = ref( false )
-        const regimenfiscal   = ref( '' )
-        const rfc             = ref('')
-        const taxid           = ref( '' )
-        const ListaPaises     = ref( [] )
+
+
+
+    setup(props) {
+
+        const esextranjejo = ref(false)
+        const idempresa = ref('')
+        const listaregimenes = ref([])
+        const nombrecomercial = ref('')
+        const nombreoficial = ref('')
+        const pais = ref('')
+        const personafisica = ref(true)
+        const personamoral = ref(false)
+        const regimenfiscal = ref('')
+        const rfc = ref('')
+        const taxid = ref('')
+        const ListaPaises = ref([])
 
 
         // const ListaPaises = [
@@ -110,22 +166,24 @@ export default {
         // ]
 
 
+
+
         const ListaRegimenFiscales = [
             {
                 ClaveRegimenFiscal: '601',
-                Descripcion:'General de Ley Personas Morales',
+                Descripcion: 'General de Ley Personas Morales',
                 Fisica: 0,
                 Moral: 1
             },
             {
                 ClaveRegimenFiscal: '612',
-                Descripcion:'Personas Físicas con Actividades Empresariales y Profesionales',
+                Descripcion: 'Personas Físicas con Actividades Empresariales y Profesionales',
                 Fisica: 1,
                 Moral: 0
             },
             {
                 ClaveRegimenFiscal: '616',
-                Descripcion:'Sin obligaciones fiscales',
+                Descripcion: 'Sin obligaciones fiscales',
                 Fisica: 1,
                 Moral: 0
             },
@@ -133,51 +191,51 @@ export default {
 
         const store = useEmpresa()
 
-        onMounted(()=>{
+        onMounted(() => {
             //listaregimenes.value = ListaRegimenFiscales.filter( regimen => regimen.Fisica === 1)
 
-            
-            store.cargarPaises().then(()=>{
+
+            store.cargarPaises().then(() => {
                 ListaPaises.value = store.listapaises
-                
+
             })
 
-            store.cargarRegimenes().then( ()=>{
+            store.cargarRegimenes().then(() => {
                 listaregimenes.value = store.listaPFisica
 
             })
-            
-            
-        })   
-        
-        
 
-        const PersonaSelecionada = (valor)=>{
+
+        })
+
+
+
+        const PersonaSelecionada = (valor) => {
             personafisica.value = valor
             personamoral.value = !personafisica.value
-           
+
         }
 
-        watch( pais, ( pais ) => {
+        watch(pais, (pais) => {
             esextrangejo.value = false
-            if( pais.length>0 && pais!=='MEX' ){
+            if (pais.length > 0 && pais !== 'MEX') {
                 esextrangejo.value = !esextrangejo.value
                 personafisica.value = true
             }
         })
 
-        watch( personafisica, ( fisica )=>{
-            
-            if(fisica){
+        watch(personafisica, (fisica) => {
+
+            if (fisica) {
                 // listaregimenes.value = ListaRegimenFiscales.filter( regimen => regimen.Fisica === 1)
                 listaregimenes.value = store.listaPFisica
-            }else{
+            } else {
                 //listaregimenes.value = ListaRegimenFiscales.filter( regimen => regimen.Fisica === 0)
                 listaregimenes.value = store.listaPMoral
             }
         })
-        
-        return{
+
+        return {
             esextranjejo,
             idempresa,
             ListaPaises,
@@ -190,22 +248,23 @@ export default {
             regimenfiscal,
             rfc,
             taxid,
-            
-        
+
+
             PersonaSelecionada,
 
         }
 
     }
-    
+
 }
+
 </script>
 
 <style lang="scss" scoped>
-    @import '../../../styles/variables.scss';
+@import '../../../styles/variables.scss';
 
 
-.frame{
+.frame {
     align-items: left;
     background-color: $panel;
     height: 43.5rem;
@@ -214,42 +273,42 @@ export default {
     width: 40.93rem;
 }
 
-h1{
+h1 {
     color: #fff;
     font-size: 2rem;
     text-align: left;
 }
 
-fieldset{
+fieldset {
     margin-top: .5rem;
     margin-bottom: .5rem;
 }
 
-.idEmpresa{
+.idEmpresa {
     border-radius: $radius;
     border: none;
     height: 2.02375rem;
     padding: $padding-input;
     width: 9.0625rem;
-    color:#999999
+    color: #999999
 }
 
-.logotipo{
+.logotipo {
     font-size: 1.125rem;
     margin-left: .5rem;
     margin-right: .5rem;
 }
 
-.rfc{
+.rfc {
     border-radius: $radius;
     border: none;
     height: 2.02375rem;
     padding: $padding-input;
     width: 9.0625rem;
-    color:#999999
+    color: #999999
 }
 
-.nombreOficial{
+.nombreOficial {
     border-radius: $radius;
     border: none;
     height: 2.02375rem;
@@ -257,15 +316,16 @@ fieldset{
     margin-right: 8px;
     padding: $padding-input;
     width: 26.9999rem;
-    color:#999999
+    color: #999999
 }
 
-.labelPais{
+.labelPais {
     font-size: 1.125rem;
     margin-left: .5rem;
     margin-right: 6.3125rem;
 }
-.pais{
+
+.pais {
     border-radius: $radius;
     border: none;
     height: 2.02375rem;
@@ -276,11 +336,17 @@ fieldset{
     color: #999999;
 }
 
-.labelPersonas{
+.labelPersonas {
     margin-right: 2.0625rem;
 }
 
-.taxId{
+.labelEstados {
+    font-size: 1.125rem;
+    margin-left: .5rem;
+    margin-right: .5rem;
+}
+
+.taxId {
     border-radius: $radius;
     border: none;
     height: 2.02375rem;
@@ -289,36 +355,135 @@ fieldset{
     color: #999999;
 }
 
-.Persona{
+.Persona {
     margin-right: .5rem;
     width: 1.01rem;
 }
 
-.claveRegimenFiscal{
+.claveRegimenFiscal {
     border-radius: $radius;
     border: none;
     height: 2.02375rem;
     margin-right: .5rem;
     padding: $padding-input;
     width: 36.55rem;
-    color:#999999
-}
-
-.nombreComercial{
-    border-radius: $radius;
-    border: none;
-    height: 2.02375rem;
-    margin-right: .5rem;
-    padding: $padding-input;
-    width: 36.55rem;
-    color:#999999
-}
-
-select{
     color: #999999
 }
 
-option{
+.claveEstado {
+    border-radius: $radius;
+    border: none;
+    height: 2.02375rem;
+    margin-right: .5rem;
+    padding: $padding-input;
+    width: 22.72rem;
+    color: #999999
+}
+
+.nombreComercial {
+    border-radius: $radius;
+    border: none;
+    height: 2.02375rem;
+    margin-right: .5rem;
+    padding: $padding-input;
+    width: 36.55rem;
+    color: #999999
+}
+
+.calleDomicilio {
+    border-radius: $radius;
+    border: none;
+    height: 2.02375rem;
+    margin-right: .5rem;
+    padding: $padding-input;
+    width: 19.125rem;
+    color: #999999
+}
+
+.numExtDomicilio {
+    border-radius: $radius;
+    border: none;
+    height: 2.02375rem;
+    margin-right: .5rem;
+    padding: $padding-input;
+    width: 8.0625rem;
+    color: #999999
+}
+
+
+
+.numIntDomicilio {
+    border-radius: $radius;
+    border: none;
+    height: 2.02375rem;
+    margin-right: .5rem;
+    padding: $padding-input;
+    width: 8.0625rem;
+    color: #999999
+}
+
+.codigoPostal {
+    border-radius: $radius;
+    border: none;
+    height: 2.02375rem;
+    padding: $padding-input;
+    width: 9.0625rem;
+    color: #999999
+}
+
+.labelMunicipio {
+    font-size: 1.125rem;
+    margin-right: 0.65rem;
+}
+
+.municipio {
+    border-radius: $radius;
+    border: none;
+    height: 2.02375rem;
+    margin-left: 8px;
+    margin-right: 8px;
+    padding: $padding-input;
+    width: 26.80rem;
+    color: #999999
+}
+
+.labelCiudad {
+    font-size: 1.125rem;
+    margin-right: 5.4rem;
+}
+
+.ciudad {
+    border-radius: $radius;
+    border: none;
+    height: 2.02375rem;
+    margin-left: 8px;
+    margin-right: 8px;
+    padding: $padding-input;
+    width: 26.80rem;
+    color: #999999
+}
+
+.labelColonia {
+    font-size: 1.125rem;
+    margin-right: 5.2rem;
+}
+
+.colonia {
+    border-radius: $radius;
+    border: none;
+    height: 2.02375rem;
+    margin-left: 8px;
+    margin-right: 8px;
+    padding: $padding-input;
+    width: 26.80rem;
+    color: #999999
+}
+
+select {
+    color: #999999
+}
+
+option {
     color: #999999;
 }
 </style>
