a3cd76d82ce0fa77f2ece1ed77a9ac68bcb339ba
cahuichLuis <cawy2000@hotmail.com>
2023-12-01 08:56:53 -0500

    cambios totales

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,7 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-
-    <div class="card bg-dark m-5">
-      <div class="card-header"></div>
-      <div class="card-body">
-        <h1 class="text-light">hola</h1>
-      </div>
-    </div>
   </div>
 </template>
 
