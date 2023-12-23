3c8ed677da5e07078fb33e79ba92ff26f6dd99c9
Horacio <151089759+devmfcancun@users.noreply.github.com>
2023-12-22 19:39:49 -0500

    feat(domicilio) funcionalidades de domicilio

--- src/main.js
+++ src/main.js
@@ -6,7 +6,10 @@ import router from './router'
 
 import './styles/styles.scss'
 
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
+
 const pinia = createPinia()
 
 
-createApp(App).use(router).use(pinia).mount('#app')
+createApp(App).use(router).use(pinia).use(VueSweetalert2).mount('#app')
