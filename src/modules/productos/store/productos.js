6b9b59f0306c64beb0f5c4c737851253b67b05cd
LaloG19 <21362134@utcancun.edu.mx>
2024-03-21 11:49:20 -0500

    fix: action duplicado eliminado en el store de productos

--- src/modules/productos/store/productos.js
+++ src/modules/productos/store/productos.js
@@ -42,7 +42,7 @@ export const useProductos = defineStore('Productos',{
                 });
             }
         },
-        async crearProducto(producto, tipo){
+/*         async crearProducto(producto, tipo){
             try{
                 let body = {
                     "CodigoProducto": "",
@@ -71,6 +71,26 @@ export const useProductos = defineStore('Productos',{
                     Swal.fire({
                         title: "Producto creado",
                         text: `${datos.message}: ${datos.response.CodigoProducto}`,
+                        icon: "success",
+                    });
+                    return true;
+                }
+            }catch (error){
+                console.log(error);
+                Swal.fire({
+                    title: "Error",
+                    text: JSON.stringify(error.message),
+                    icon: "error",
+                });
+            }
+        }, */
+        async crearProducto(producto, tipo){
+            try{
+                const pet = await axios.post(`http://lachosoft.cloud:7000/api/v1/productos/${tipo}`, producto);
+                if(pet.status === 200 && pet.statusText === "OK"){
+                    Swal.fire({
+                        title: "Producto creado",
+                        text: `${pet.data.message}: ${pet.data.response[0].CodigoProducto}`,
                         icon: "success",
                     });
                     return true;
@@ -125,13 +145,60 @@ export const useProductos = defineStore('Productos',{
                 });
             }
         },
-        async crearProducto(producto, tipo){
+        async reactivarProducto(id){
             try{
-                const pet = await axios.post(`http://lachosoft.cloud:7000/api/v1/productos/${tipo}`, producto);
+                const p = {
+                    "CodigoProducto": id,
+                    "ActualizadoPor": 2
+                }
+                const datos = await axios.put(`http://lachosoft.cloud:7000/api/v1/productos/activar`, p);
+                if(datos.status === 200 && datos.statusText === "OK"){
+                    Swal.fire({
+                        title: `${datos.data.message}`,
+                        text: `Clave producto: ${id}`,
+                        icon: "success",
+                    });
+                    return true;
+                }
+            }catch (error){
+                console.log(error);
+                Swal.fire({
+                    title: "Error",
+                    text: JSON.stringify(error.message),
+                    icon: "error",
+                });
+            }
+        },
+        async actualizarProducto(t, producto){
+            try{
+                const id = producto.CodigoProducto;
+                const datos = await axios.put(`http://lachosoft.cloud:7000/api/v1/productos/${t}/${id}`, producto);
+
+
+                if(datos.status === 200 && datos.statusText === "OK"){
+                    Swal.fire({
+                        title: `Producto actualizado`,
+                        text: `${datos.data.message} con id: ${id}`,
+                        icon: "success",
+                    });
+                    return true;
+                }
+            }catch (error){
+                console.log(error);
+                Swal.fire({
+                    title: "Error",
+                    text: JSON.stringify(error.message),
+                    icon: "error",
+                });
+            }
+        },
+        async crearPoliticaMembresia(politica){
+            try{
+                const pet = await axios.post(`http://lachosoft.cloud:7000/api/v1/politicasMembresia`, politica);
                 if(pet.status === 200 && pet.statusText === "OK"){
                     Swal.fire({
-                        title: "Producto creado",
-                        text: `${pet.data.message}: ${pet.data.response[0].CodigoProducto}`,
+                        title: "Politica creada",
+                        text: `${pet.data.message}: ${pet.data.response[0].PoliticasMembreciasId}`,
                         icon: "success",
                     });
                     return true;
